a396c9b3e43d40f02b5cd517b2e51eff6b5c9357
Nick Talati <nicktalati@gmail.com>
2025-03-16 12:26:50 -0400

    adding v1 vs v2 specs

--- swagger-initializer.js
+++ swagger-initializer.js
@@ -8,7 +8,16 @@ const getRedirectUrl = () => {
 
 window.onload = function() {
   window.ui = SwaggerUIBundle({
-    url: 'openapi.yaml',
+    urls: [
+      {
+        name: "API v1",
+        url: "openapi-v1.yaml"
+      },
+      {
+        name: "API v2",
+        url: "openapi-v2.yaml"
+      }
+    ],
     dom_id: '#swagger-ui',
     oauth2RedirectUrl: getRedirectUrl(),
     defaultModelRendering: 'model',
